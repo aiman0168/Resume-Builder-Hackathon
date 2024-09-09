faa79f34aec58bb7dd8a1205b56a2197295d2e0a
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-09-09 10:15:23 -0700

    Add files via upload

---
+++ milestone04-RESUME-GENERATOR/script.js
@@ -0,0 +1,18 @@
+// Function to show or hide an element
+function toggleVisibility(element) {
+    if (element.style.display === 'none' || element.style.display === '') {
+        element.style.display = 'block';
+    }
+    else {
+        element.style.display = 'none';
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggle-button');
+    var toggleDiv = document.getElementById('toggle-div');
+    if (toggleButton && toggleDiv) {
+        toggleButton.addEventListener('click', function () {
+            toggleVisibility(toggleDiv);
+        });
+    }
+});
