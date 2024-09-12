d1fe7ffdb140965492c3563dac96c4b9a61ea375
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-09-12 11:45:44 -0700

    Update script.js

--- RESUME-GENERATOR/script.js
+++ RESUME-GENERATOR/script.js
@@ -1,19 +1,10 @@
-// Function to show or hide an element
-function toggleVisibility(element) {
-    if (element.style.display === 'none' || element.style.display === '') {
-        element.style.display = 'block';
+var toggleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
     }
     else {
-        element.style.display = 'none';
-    }
-}
-// Initialize event listeners after the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    var toggleButton = document.getElementById('toggle-button');
-    var toggleDiv = document.getElementById('toggle-div');
-    if (toggleButton && toggleDiv) {
-        toggleButton.addEventListener('click', function () {
-            toggleVisibility(toggleDiv);
-        });
+        skills.style.display = 'none';
     }
 });
